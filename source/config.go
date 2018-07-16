5de80a599ac280e3499127e07da5fcf1fb52a102
lizhengxiang <1065482100@qq.com>
2018-07-16 09:26:31 +0800

    update

---
+++ source/config.go
@@ -0,0 +1,24 @@
+package coinmarketcap
+
+var USD  = 6.69
+var BitcoinTotal  = 0.01204000
+var EthereumTotal  = 0.17303955
+var EosTotal  = 11.83628000
+var GnxTotal  = 1578.19490887
+
+var User  = "15308003217@163.com"
+var Password = ""
+var Host = "smtp.163.com:25"
+var To = "1065482100@qq.com"
+
+var Subject = "使用Golang发送邮件"
+
+var Body = `
+		<html>
+		<body>
+		<h3>
+		"send to email"
+		</h3>
+		</body>
+		</html>
+		`
