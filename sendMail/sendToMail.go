980d46d2da1341260da1509c1b5620cfc72820c9
lizhengxiang <1065482100@qq.com>
2018-07-18 03:37:31 -0400

    update

--- sendMail/sendToMail.go
+++ sendMail/sendToMail.go
@@ -5,6 +5,7 @@ import (
 	"net/smtp"
 	"fmt"
 	"strconv"
+	"math"
 )
 
 func SendToMail(user, password, host, to, subject, body, mailtype string) error {
@@ -35,13 +36,15 @@ func SendMail(Body string)  {
 
 
 func MailTemplate(diff float64,cointype int){
-	float32s2 := strconv.FormatFloat(diff, 'f', -1, 64)//float64
+	pow10_n := math.Pow10(2)
+	diffResult := math.Trunc((diff+0.5/pow10_n)*pow10_n) / pow10_n
+	float32s2 := strconv.FormatFloat(diffResult, 'f', -1, 64)//float64
 	fmt.Println(float32s2)
 	var Body = `
 		<html>
 			<body>
 				<h3>
-					`+"cointype" + strconv.Itoa(cointype)+"price fluctuation"+float32s2+`
+					`+"coin type " + strconv.Itoa(cointype)+" price fluctuation"+float32s2+`
 				</h3>
 			</body>
 		</html>
